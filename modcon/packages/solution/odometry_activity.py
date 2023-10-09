4bf17af858e5fb4c2f6e30935e9fb0f463d4c9a8
Dominik Koller <mail@dominikkoller.com>
2023-10-09 12:34:53 +0200

    modcon odometry activity

--- modcon/packages/solution/odometry_activity.py
+++ modcon/packages/solution/odometry_activity.py
@@ -14,10 +14,11 @@ def delta_phi(ticks: int, prev_ticks: int, resolution: int) -> Tuple[float, floa
         ticks: current number of ticks.
     """
 
-    # TODO: these are random values, you have to implement your own solution in here
-    ticks = prev_ticks + int(np.random.uniform(0, 10))
-    dphi = np.random.random()
-    # ---
+    alpha = 2 * np.pi / resolution # wheel rotation per tick in radians
+
+    delta_ticks = ticks - prev_ticks
+    dphi = 2 * np.pi * delta_ticks / resolution
+
     return dphi, ticks
 
 
@@ -50,9 +51,33 @@ def pose_estimation(
         theta:              estimated heading
     """
 
+    L = baseline / 2
+
+    try:
+        delta_q = (R/2) * np.array([
+                [np.cos(theta_prev), 0],
+                [np.sin(theta_prev), 0],
+                [0, 1]
+            ]) @ np.array([
+                [1, 1],
+                [1/L, -1/L]
+            ]) @ np.array([
+                [delta_phi_right],
+                [delta_phi_left]
+            ])
+    except:
+        print("exception")
+        print("R: " + str(R))
+        print("baseline: " + str(baseline))
+        print("x_prev: " + str(x_prev))
+        print("y_prev: " + str(y_prev))
+        print("theta_prev: " + str(theta_prev))
+        print("delta_phi_left: " + str(delta_phi_left))
+        print("delta_phi_right: " + str(delta_phi_right))
+
     # These are random values, replace with your own
-    x_curr = np.random.random()
-    y_curr = np.random.random()
-    theta_curr = np.random.random()
+    x_curr = x_prev + delta_q[0][0]
+    y_curr = y_prev + delta_q[1][0]
+    theta_curr = theta_prev + delta_q[2][0]
     # ---
     return x_curr, y_curr, theta_curr
