ab984a320cc5e17d80900add3d8883cb7fc761fe
Dominik Koller <mail@dominikkoller.com>
2023-10-11 21:43:12 +0200

    visual improvement WIP

--- visual-lane-servoing/packages/solution/visual_servoing_activity.py
+++ visual-lane-servoing/packages/solution/visual_servoing_activity.py
@@ -14,7 +14,11 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
                             using the masked left lane markings (numpy.ndarray)
     """
 
-    return gradient_array(shape, from_=0, to=-1, angle=180)
+    result = gradient_array(shape, from_=-0.5, to=-1, angle=180)
+
+    result = result * gradient_array(shape, from_=0.5, to=1, angle=90)
+
+    return result
 
 
 def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
@@ -28,14 +32,17 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     """
 
     rows, cols = shape
-    gradient_start_col = cols // 2
-    combined = np.zeros(shape)
+    gradient_start_col = cols // 3
+    result = np.zeros(shape)
     
     gradient_shape = (rows, cols - gradient_start_col)
-    gradient_part = gradient_array(gradient_shape, from_=1, to=0, angle=0)
+    gradient_part = gradient_array(gradient_shape, from_=1, to=0.5, angle=0)
     
-    combined[:, gradient_start_col:] = gradient_part
-    return combined
+    result[:, gradient_start_col:] = gradient_part
+
+    result = result * gradient_array(shape, from_=0.5, to=1, angle=90)
+
+    return result
 
 
 def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -55,13 +62,22 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     yellow_lower_hsv = np.array([26, 84, 176]) - tolerance
     yellow_upper_hsv = np.array([29, 240, 200]) + tolerance
 
-    mask_white = cv2.inRange(imghsv, white_lower_hsv, white_upper_hsv)
-    mask_yellow = cv2.inRange(imghsv, yellow_lower_hsv, yellow_upper_hsv)
+    masked_white = cv2.inRange(imghsv, white_lower_hsv, white_upper_hsv)
+    masked_yellow = cv2.inRange(imghsv, yellow_lower_hsv, yellow_upper_hsv)
 
-    mask_left_edge = mask_yellow
-    mask_right_edge = mask_white
+    return lane_markings_from_masked(masked_yellow), lane_markings_from_masked(masked_white)
 
-    return mask_left_edge, mask_right_edge
+def lane_markings_from_masked(image):
+    sigma = 1
+    image = cv2.GaussianBlur(image,(0,0), sigma)
+
+    sobelx = cv2.Sobel(image,cv2.CV_64F,1,0)
+    sobely = cv2.Sobel(image,cv2.CV_64F,0,1)
+
+     # Compute the magnitude of the gradients
+    Gmag = np.sqrt(sobelx*sobelx + sobely*sobely)
+
+    return Gmag # (Gmag > 20)
 
 def gradient_array(shape, from_, to, angle):
     # Create a grid of x and y coordinates
